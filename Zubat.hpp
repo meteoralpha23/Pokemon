72841c8ab79678d7bccb997b271b6adfa1ac9141
meteoralpha23 <luckycreed123@gmail.com>
2024-10-03 06:58:11 +0530

    2

--- Zubat.hpp
+++ Zubat.hpp
@@ -5,6 +5,9 @@ namespace N_Pokemon {
     namespace N_Pokemons {
 
         class Zubat : public Pokemon {
+        public:
+            Zubat();
+            void supersonic(Pokemon* target);
         };
 
     }
